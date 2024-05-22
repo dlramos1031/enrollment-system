5a811711a9f78c926c2a7aee098cc7461cf94bd0
dlramos1031 <74926928+dlramos1031@users.noreply.github.com>
2024-05-22 09:02:54 +0800

    Installing React, Vite, and Tailwind CSS

---
+++ bm-enrollment/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold text-red-500 underline'>Hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
